ef643916cba59d5b1cae2c30b642b837a55aee4f
carozh91 <165485810+carozh91@users.noreply.github.com>
2026-02-17 21:26:02 -0500

    First operation

---
+++ OPPConcepts/OPPConcepts.Backend/Date.cs
@@ -0,0 +1,12 @@
+﻿namespace OPPConcepts.Backend;
+
+public class Date
+{
+    private int _year;
+    private int _month;
+    private int _day;
+
+    public int Year { get; set; }
+    public int Month { get; set; }
+    public int Day { get; set;  }
+}
