8dad4625de72f8194c7c4296e5c449a15aaf2fd6
carozh91 <165485810+carozh91@users.noreply.github.com>
2026-02-18 19:44:31 -0500

    Finished exercise1

--- OPPConcepts/OPPConcepts.Backend/Date.cs
+++ OPPConcepts/OPPConcepts.Backend/Date.cs
@@ -1,4 +1,7 @@
-﻿namespace OPPConcepts.Backend;
+﻿using Microsoft.VisualBasic;
+using System.Runtime.CompilerServices;
+
+namespace OPPConcepts.Backend;
 
 public class Date
 {
@@ -6,7 +9,91 @@ public class Date
     private int _month;
     private int _day;
 
-    public int Year { get; set; }
-    public int Month { get; set; }
-    public int Day { get; set;  }
+    public Date()
+    {
+        _year = 1900;
+        _month = 1;
+        _day = 1;
+    }
+
+    public Date(int year, int month, int day)
+    {
+        Year = year;
+        Month = month;
+        Day = day;
+    }
+
+    public int Year
+    {
+        get => _year;
+        set => _year = ValidateYear(value);
+    }
+    public int Month
+    {
+        get => _month;
+        set => _month = ValidateMonth(value);
+    }
+    public int Day
+    {
+        get => _day;
+        set => _day = value;
+    }
+
+    public override string ToString()
+    {
+        return $"{Year:0000}/{Month:00}/{Day:00}";
+    }
+
+    private int ValidateYear(int year)
+    {
+        if (year < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(year), "Year must be greatest than 0");
+
+        }
+
+        return year;
+
+    }
+
+    private int ValidateMonth(int month)
+    {
+        if (month < 1 || month > 12)
+        {
+            throw new ArgumentOutOfRangeException(nameof(month), "Month must be between 1 and 12");
+
+        }
+
+        return month;
+
+    }
+
+    private int ValidateDay(int day)
+    {
+        if (day < 1)
+
+        {
+            throw new ArgumentOutOfRangeException(nameof(day), "Day must be greatest than 0");
+        }
+        if (day == 29 && Month == 2 && IsLeapYear(Year))
+        {
+            return day;
+        }
+        int[] daysInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        if (day > daysInMonth[Month - 1])
+        {
+            throw new ArgumentOutOfRangeException(nameof(day), $"Day must be between 1 and {daysInMonth[Month - 1]}and for month {Month}");
+        }
+
+        return day;
+    }
+
+
+    private bool IsLeapYear(int year)
+    {
+        return (year % 4 == 0 && year % 100 != 0 )|| (year % 400 == 0);
+    }
+
+
+
 }
---
+++ OPPConcepts/OPPConcepts.Frontend/Program.cs
@@ -0,0 +1,16 @@
+﻿using OPPConcepts.Backend;
+
+try
+{
+    var date1 = new Date();
+    var date2 = new Date(2026, 2, 12);
+
+    Console.WriteLine(date1);
+    Console.WriteLine(date2);
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
+
