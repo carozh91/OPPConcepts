77d381ec06367e826bb62b6d3a2bac2348b3e287
carozh91 <165485810+carozh91@users.noreply.github.com>
2026-03-07 11:51:19 -0500

    Trapeze Created

---
+++ Talleres/Taller02.Backend/Trapeze.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace Taller02.Backend;
+
+public class Trapeze:Triangle
+{
+    private decimal _d;
+    
+    public Trapeze(string name, decimal a, decimal b, decimal h, decimal c,decimal d) : base(name,a,b,h,c)
+    {
+        D = d;
+    }
+
+    public decimal D
+    {
+        get => _d;
+        set => _d = ValidateD(value);
+    }
+    public override decimal GetArea() => (B + D) * H/2;
+
+
+    public override decimal GetPerimiter() => (A + B + C + D);
+
+    private decimal ValidateD(decimal d)
+    {
+        if (d < 0)
+        {
+            throw new ArgumentException(nameof(d), "D must be gratest than 0");
+        }
+        return d;
+    }
+
+
+}
