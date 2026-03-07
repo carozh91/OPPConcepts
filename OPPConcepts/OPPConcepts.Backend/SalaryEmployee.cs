a61e2575ef7458bdd6a64796ba2876b3bd0e0cd6
carozh91 <165485810+carozh91@users.noreply.github.com>
2026-03-06 19:03:36 -0500

    Add SalaryEmployee

---
+++ OPPConcepts/OPPConcepts.Backend/SalaryEmployee.cs
@@ -0,0 +1,34 @@
+﻿
+namespace OPPConcepts.Backend;
+
+public class SalaryEmployee : Employee
+{
+
+    private decimal _salary;
+    public SalaryEmployee(int id, string firstName, string lastName, bool isActive, Date bornDate, Date hireDate, decimal salary):
+        base(id, firstName, lastName, isActive, bornDate, hireDate)
+    {
+        Salary = salary;
+    }
+
+    public decimal Salary 
+    {   
+        get => _salary; 
+        set => _salary = ValidateSalary(value); 
+    }
+
+    public override decimal GetValueToPay() => Salary;
+
+    public override string ToString() => base.ToString();
+
+    private decimal ValidateSalary(decimal salary)
+    {
+        if (salary < 2000000) 
+        {
+            throw new ArgumentOutOfRangeException(nameof(salary), "Salary must be gratest o equal than 2000000");
+        }
+        return salary;
+    }   
+
+
+}
