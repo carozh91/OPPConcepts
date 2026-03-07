445938971a868da208190924a04cdb807a33dd21
carozh91 <165485810+carozh91@users.noreply.github.com>
2026-03-07 11:20:18 -0500

    Kite Created

---
+++ Talleres/Taller02.Backend/Kite.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace Taller02.Backend;
+
+public class Kite : Rhombus
+
+    
+{
+    private decimal _b;
+    public Kite(string name, decimal a, decimal d1, decimal d2, decimal b): base(name,a,d1,d2)
+    {
+        B = b;
+    }
+
+    public decimal B 
+    { 
+        get => _b; 
+        set => _b = ValidateB(value); 
+    }
+    public override decimal GetArea() => (D1 * D2) / 2;
+
+
+    public override decimal GetPerimiter() => 2 * (A + B);
+   
+
+    private decimal ValidateB(decimal b)
+    {
+        if (b < 0)
+        {
+            throw new ArgumentException(nameof(b), "B must be gratest than 0");
+        }
+        return b;
+    }
+
+
+}
