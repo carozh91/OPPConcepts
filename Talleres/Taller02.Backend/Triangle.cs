086ed30a490ebb9fd0042c55c2b3f374850abb3a
carozh91 <165485810+carozh91@users.noreply.github.com>
2026-03-07 11:40:17 -0500

    Triangle Created

---
+++ Talleres/Taller02.Backend/Triangle.cs
@@ -0,0 +1,50 @@
+﻿
+
+namespace Taller02.Backend;
+
+public class Triangle:Rectangle
+{
+    private decimal _h;
+    private decimal _c;
+    public Triangle(string name, decimal a, decimal b, decimal h,decimal c) : base(name, a, b)
+    {
+        H = h;
+        C = c;
+    }
+
+    public decimal H
+    {
+        get => _h;
+        set => _h = ValidateH(value);
+    }
+
+    public decimal C
+    {
+        get => _c;
+        set => _c = ValidateC(value);
+    }
+
+
+    public override decimal GetArea() => (H * B)/2;
+
+
+    public override decimal GetPerimiter() => (A + B + C);
+
+    private decimal ValidateH(decimal h)
+    {
+        if (h < 0)
+        {
+            throw new ArgumentException(nameof(h), "H must be gratest than 0");
+        }
+        return h;
+    }
+
+    private decimal ValidateC(decimal c)
+    {
+        if (c < 0)
+        {
+            throw new ArgumentException(nameof(c), "C must be gratest than 0");
+        }
+        return c;
+    }
+}
