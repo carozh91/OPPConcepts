dc5297f586b2a0a7990adc2ee3257387d66c1128
carozh91 <165485810+carozh91@users.noreply.github.com>
2026-03-07 11:35:43 -0500

    Parallelogram Cretaed

---
+++ Talleres/Taller02.Backend/Parallelogram.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Taller02.Backend;
+
+public class Parallelogram: Rectangle
+{
+    private decimal _h;
+    public Parallelogram(string name, decimal a, decimal b, decimal h) : base(name, a,b)
+    {
+        h = _h;
+    }
+
+    public decimal H
+    {
+        get => _h;
+        set => _h = ValidateH(value);
+    }
+    public override decimal GetArea() => H * B;
+
+
+    public override decimal GetPerimiter() => 2 * (A + B);
+
+    private decimal ValidateH(decimal h)
+    {
+        if (h < 0)
+        {
+            throw new ArgumentException(nameof(h), "B must be gratest than 0");
+        }
+        return h;
+    }
+}
