ab483fb3c0bc05c7142beb0385eea2d8367a09f0
carozh91 <165485810+carozh91@users.noreply.github.com>
2026-03-07 11:28:03 -0500

    Rectangle Created

---
+++ Talleres/Taller02.Backend/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Taller02.Backend;
+
+public class Rectangle : Square
+{
+    private decimal _b;
+    public Rectangle(string name, decimal a, decimal b): base(name,a)
+    {
+        B = b;
+    }
+
+    public decimal B
+    {
+        get => _b;
+        set => _b = ValidateB(value);
+    }
+    public override decimal GetArea() => A * B;
+
+
+    public override decimal GetPerimiter() => 2 * (A + B);
+
+    private decimal ValidateB(decimal b)
+    {
+        if (b < 0)
+        {
+            throw new ArgumentException(nameof(b), "B must be gratest than 0");
+        }
+        return b;
+    }
+}
